8dd3e365e5f5dada98a09a5eed111ba2ede75ba8
Javier <japermar@yahoo.com>
2024-02-13 19:11:32 -0500

    configurado mailtrap para emails

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestEmail;
+
+Route::get('/send-test-email', function () {
+    $to = 'test@example.com'; // The recipient of the email
+    Mail::to($to)->send(new TestEmail());
+
+    return 'Email sent successfully!';
+});
