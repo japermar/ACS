61be56db63c6f95d23e2d8f0038436b7e3be7f25
Javier <japermar@yahoo.com>
2024-02-14 19:06:04 -0500

    modificado gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         }(window, document);</script>
     <link href="https://assets-global.website-files.com/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
     <link href="https://assets-global.website-files.com/img/webclip.png" rel="apple-touch-icon">
-    <script defer src="{{ asset('js/interactivity.js') }}" type="text/javascript" ></script>
+    <script defer src="{{ asset('js/interactivity.js') }}" type="text/javascript"></script>
 
     <style>
         .lightbox-modal {
@@ -43,7 +43,7 @@
         <div class="navbar-container"><a href="/"
                                          aria-current="page" class="navbar-logo-link w-nav-brand w--current">
                 <div class="logo-img w-embed">
-                    <img src="{{asset('/img/logo.webp')}}" alt="" width="92" height="92" >
+                    <img src="{{asset('/img/logo.webp')}}" alt="" width="92" height="92">
                 </div>
             </a>
             <nav role="navigation" class="navbar-menu overflow-hidden w-nav-menu"><a
@@ -72,7 +72,9 @@
                     <div class="spacer_xs"></div>
                     <h1 class="h1">Configuracion de Sistemas Simple</h1>
                     <div class="spacer_xs"></div>
-                    <p class="paragraph">ACS (Auto configuracion de sistemas) es una novedosa idea donde podras administrar tu VPS de una forma grafica comoda e intuitia sin necesidad de recordar comandos.</p>
+                    <p class="paragraph">ACS (Auto configuracion de sistemas) es una novedosa idea donde podras
+                        administrar tu VPS de una forma grafica comoda e intuitia sin necesidad de recordar
+                        comandos.</p>
                     <div class="spacer_l"></div>
                     <a href="#" class="button w-button">Registrate</a></div>
                 <img src="{{asset('img/nube.jpg')}}" loading="lazy"
@@ -83,11 +85,13 @@
     <div class="main-section">
         <div class="main-container">
             <div class="w-layout-grid grid_1-2">
-                <div id="w-node-_9a521c9b-b9ee-4177-6591-f2e194d4a0fd-2c4ffd25" class="content-wrap"><h2 class="h2">Hecha para todo tipo de pantallas</h2>
+                <div id="w-node-_9a521c9b-b9ee-4177-6591-f2e194d4a0fd-2c4ffd25" class="content-wrap"><h2 class="h2">
+                        Hecha para todo tipo de pantallas</h2>
                     <div class="spacer_m"></div>
-                    <p class="paragraph">Con nuestro servicio puedes configurar un VPS desde el movil gracias a la sencillez grafica.</p>
+                    <p class="paragraph">Con nuestro servicio puedes configurar un VPS desde el movil gracias a la
+                        sencillez grafica.</p>
                     <div class="spacer_l"></div>
-                 </div>
+                </div>
                 <div id="w-node-_8e653e4b-67f9-f2a7-6f82-d80e04f3535f-2c4ffd25" class="macbook-wrapper"><img
                         src="{{asset('img/screen.jpg')}}" loading="lazy"
                         id="w-node-_8e653e4b-67f9-f2a7-6f82-d80e04f35360-2c4ffd25" sizes="90vw" alt=""
@@ -104,9 +108,11 @@
                                                              loading="lazy"
                                                              id="w-node-b4ef11d3-2ae9-1808-aaa2-cbc9973a0fbb-2c4ffd25"
                                                              sizes="90vw" alt="" class="hero-image">
-                <div id="w-node-b4ef11d3-2ae9-1808-aaa2-cbc9973a0fbc-2c4ffd25" class="content-wrap"><h2 class="h2">Tenemos un gran objetivo</h2>
+                <div id="w-node-b4ef11d3-2ae9-1808-aaa2-cbc9973a0fbc-2c4ffd25" class="content-wrap"><h2 class="h2">
+                        Tenemos un gran objetivo</h2>
                     <div class="spacer_m"></div>
-                    <p class="paragraph">Nuestro objetivo es conseguir que el despliegue de aplicaciones web o configuraciones de servidores sea lo mas comoda y facil posible .</p>
+                    <p class="paragraph">Nuestro objetivo es conseguir que el despliegue de aplicaciones web o
+                        configuraciones de servidores sea lo mas comoda y facil posible .</p>
                     <div class="spacer_l"></div>
                     <a href="#" class="button secondary w-button">Crear cuenta</a></div>
             </div>
@@ -119,46 +125,52 @@
                      data-w-id="896be899-d6d5-78ce-59df-eb1d6476f4e1" style="opacity:0" class="content-wrap"><h2
                         class="h2">Todo lo que buscas en un solo lugar.</h2>
                     <div class="spacer_m"></div>
-                    <p class="paragraph">Queremos que ACS sea un sistema completo a la hora de desplegar para no tener que utilizar otras herramientas.</p>
+                    <p class="paragraph">Queremos que ACS sea un sistema completo a la hora de desplegar para no tener
+                        que utilizar otras herramientas.</p>
                     <div class="spacer_l"></div>
-                   </div>
+                </div>
                 <div id="w-node-_896be899-d6d5-78ce-59df-eb1d6476f4ea-2c4ffd25" class="w-layout-grid grid_2-2">
                     <div id="w-node-_896be899-d6d5-78ce-59df-eb1d6476f4eb-2c4ffd25"
                          data-w-id="896be899-d6d5-78ce-59df-eb1d6476f4eb" style="opacity:0" class="card_small">
                         <div class="icon w-embed">
-                           <img src="{{asset('/img/logo.webp')}}">
+                            <img src="{{asset('/img/logo.webp')}}">
                         </div>
                         <div class="spacer_xs"></div>
                         <h3 class="h3">Ligero</h3>
                         <div class="spacer_xs"></div>
-                        <p class="paragraph">ACS se centra en ser rapido y ligero para llevar a cabo una tarea lo mas rapido posible.</p></div>
+                        <p class="paragraph">ACS se centra en ser rapido y ligero para llevar a cabo una tarea lo mas
+                            rapido posible.</p></div>
                     <div id="w-node-_896be899-d6d5-78ce-59df-eb1d6476f4f6-2c4ffd25"
                          data-w-id="896be899-d6d5-78ce-59df-eb1d6476f4f6" style="opacity:0" class="card_small">
                         <div class="icon w-embed">
-                           <img src="{{asset('/img/logo.webp')}}">
+                            <img src="{{asset('/img/logo.webp')}}">
                         </div>
                         <div class="spacer_xs"></div>
                         <h3 class="h3">Comodo</h3>
                         <div class="spacer_xs"></div>
-                        <p class="paragraph">La sencillez visual permite mejorar la concentracion a la hora de configurar un VPS.</p></div>
+                        <p class="paragraph">La sencillez visual permite mejorar la concentracion a la hora de
+                            configurar un VPS.</p></div>
                     <div id="w-node-_896be899-d6d5-78ce-59df-eb1d6476f501-2c4ffd25"
                          data-w-id="896be899-d6d5-78ce-59df-eb1d6476f501" style="opacity:0" class="card_small">
                         <div class="icon w-embed">
-                           <img src="{{asset('/img/logo.webp')}}">
+                            <img src="{{asset('/img/logo.webp')}}">
                         </div>
                         <div class="spacer_xs"></div>
                         <h3 class="h3">Eficiencia Energética</h3>
                         <div class="spacer_xs"></div>
-                        <p class="paragraph">Minimiza los costos operativos y el consumo de recursos gracias a una gestión eficiente, automatizada y optimizada de tus servidores.</p></div>
+                        <p class="paragraph">Minimiza los costos operativos y el consumo de recursos gracias a una
+                            gestión eficiente, automatizada y optimizada de tus servidores.</p></div>
                     <div id="w-node-_896be899-d6d5-78ce-59df-eb1d6476f50c-2c4ffd25"
                          data-w-id="896be899-d6d5-78ce-59df-eb1d6476f50c" style="opacity:0" class="card_small">
                         <div class="icon w-embed">
-                           <img src="{{asset('/img/logo.webp')}}">
+                            <img src="{{asset('/img/logo.webp')}}">
                         </div>
                         <div class="spacer_xs"></div>
                         <h3 class="h3">Optimización con Inteligencia Artificial</h3>
                         <div class="spacer_xs"></div>
-                        <p class="paragraph">Imagina tener un asistente personal que no solo entiende tus necesidades sino que también ofrece soluciones proactivas, gracias a la integración con la API de OpenAI.</p></div>
+                        <p class="paragraph">Imagina tener un asistente personal que no solo entiende tus necesidades
+                            sino que también ofrece soluciones proactivas, gracias a la integración con la API de
+                            OpenAI.</p></div>
                 </div>
             </div>
         </div>
@@ -172,21 +184,21 @@
                     <div class="spacer_xs"></div>
                     <p class="paragraph">Se termino depender de miles de servicios para manejar un servidor.</p></div>
 
-                </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="footer-outer-wrap">
-        <div class="footer_2-inner"><a href="#" class="navbar-logo-link w-nav-brand">
-                <div class="logo-img w-embed">
-                    <img src="{{asset('/img/logo.webp')}}" alt="">
-                </div>
-            </a>
-            <div class="footer-credit-text">© {{ now()->year  }}  {{ config('app.name', 'Laravel') }}</div>
+<div class="footer-outer-wrap">
+    <div class="footer_2-inner"><a href="#" class="navbar-logo-link w-nav-brand">
+            <div class="logo-img w-embed">
+                <img src="{{asset('/img/logo.webp')}}" alt="">
             </div>
-
+        </a>
+        <div class="footer-credit-text">© {{ now()->year  }}  {{ config('app.name', 'Laravel') }}</div>
     </div>
+
+</div>
 </div>
 <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=65c2d1ec8547d6ac2c4ffc9f"
         type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
