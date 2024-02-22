f026fec2a4278f13c536c5fa54c164c89a9d4b76
Japermar <japermar@yahoo.com>
2024-02-21 21:20:23 -0500

    implementado logica para conectarse por ssh al vps

--- app/Http/Controllers/EspecificoController.php
+++ app/Http/Controllers/EspecificoController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HardwareServidor;
 use App\Models\Servidor;
 use Illuminate\Http\Request;
-
+use Spatie\Ssh\Ssh;
+use Symfony\Component\Process\Process;
 class EspecificoController extends Controller
 {
 
@@ -23,7 +25,34 @@ class EspecificoController extends Controller
             return redirect()->back()->withErrors(['error' => "No tienes acceso al grupo " . $grupo_id]);
         }
         $esAdmin = $user->esAdmin($grupo_id);
-        return view('vps.especifico', compact('esAdmin'));
+            $hardware = HardwareServidor::where('servidor_id', $vps_id)->first();
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'ssh_key_');
+        file_put_contents($tempFilePath, $privateKeyContent);
+        chmod($tempFilePath, 0600);
+        try {
+            $process = Ssh::create($usuarioVPS, $ip)
+                ->usePrivateKey($tempFilePath)->disableStrictHostKeyChecking()->execute('ping -c 4 8.8.8.8');
+
+            // Wait for the process to finish
+            $process->wait();
+
+            // Check if the process was successful
+            if ($process->isSuccessful()) {
+                // Get the output
+                $result = $process->getOutput();
+            } else {
+                // Get the error output
+                $result = 'Error: ' . $process->getErrorOutput();
+            }
+
+            // Debug/Dump the result
+//            dd($result);
+        } finally {
+            // Securely delete the temporary file
+            unlink($tempFilePath);
+        }
+
+        return view('vps.especifico', compact('esAdmin', 'hardware', 'result'));
     }
 
 
